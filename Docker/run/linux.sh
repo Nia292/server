e10f7f2a337c5c8fe8fe3868d757c84f09229532
Sebastian Erik Bauer <dataisloading@outlook.com>
2025-08-25 06:35:02 +0200

    Attempt a permission fix

--- Docker/run/linux.sh
+++ Docker/run/linux.sh
@@ -30,26 +30,26 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if $START; then
   if $STANDALONE; then
     echo "🚀 Starting in Standalone mode..."
-    docker compose -f "$SCRIPT_DIR/compose/mare-standalone.yml" -p standalone up -d
+    docker compose -f "$SCRIPT_DIR/compose/sinus-standalone.yml" -p standalone up -d
   elif $SHARDED; then
     echo "🚀 Starting in Sharded mode..."
-    docker compose -f "$SCRIPT_DIR/compose/mare-sharded.yml" -p sharded up -d
+    docker compose -f "$SCRIPT_DIR/compose/sinus-sharded.yml" -p sharded up -d
   fi
 elif $STOP; then
   if $STANDALONE; then
     echo "🛑 Stopping Standalone service..."
-    docker compose -f "$SCRIPT_DIR/compose/mare-standalone.yml" -p standalone stop
+    docker compose -f "$SCRIPT_DIR/compose/sinus-standalone.yml" -p standalone stop
   elif $SHARDED; then
     echo "🛑 Stopping Sharded service..."
-    docker compose -f "$SCRIPT_DIR/compose/mare-sharded.yml" -p sharded stop
+    docker compose -f "$SCRIPT_DIR/compose/sinus-sharded.yml" -p sharded stop
   fi
 else
   # neither -start nor -stop supplied
   if $STANDALONE; then
     echo "⚡ Running other Standalone action..."
-    docker compose -f "$SCRIPT_DIR/compose/mare-standalone.yml" -p standalone up
+    docker compose -f "$SCRIPT_DIR/compose/sinus-standalone.yml" -p standalone up
   elif $SHARDED; then
     echo "⚡ Running other Sharded action..."
-    docker compose -f "$SCRIPT_DIR/compose/mare-sharded.yml" -p sharded up
+    docker compose -f "$SCRIPT_DIR/compose/sinus-sharded.yml" -p sharded up
   fi
 fi
